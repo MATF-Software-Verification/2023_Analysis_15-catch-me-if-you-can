00b01a305980e6fe0e88276a13cd3cb4cfbb4668
Natalija9 <asanovic.natalija@gmail.com>
2024-01-30 22:09:24 +0100

    Update script for memcheck

--- valgrind/memcheck/run_memcheck.sh
+++ valgrind/memcheck/run_memcheck.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-valgrind --show-leak-kinds=all --leak-check=full --track-origins=yes --log-file="report-memecheck.txt" ./with-build/15-catch-me-if-you-can/build-catchme-Desktop_Qt_6_6_1_GCC_64bit-Debug/catchme
+valgrind --show-leak-kinds=all --leak-check=full --track-origins=yes --log-file="report-memecheck.txt" ./15-catch-me-if-you-can/build-catchme-Desktop_Qt_6_6_1_GCC_64bit-Debug/catchme
 
 echo "finished memcheck"
