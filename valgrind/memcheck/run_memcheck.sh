53891b5d31a8f8fb10edc61ec751203e3e0b515c
Natalija9 <asanovic.natalija@gmail.com>
2024-01-30 01:02:20 +0100

    Add script for memcheck

---
+++ valgrind/memcheck/run_memcheck.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+set -xe
+
+valgrind --show-leak-kinds=all --leak-check=full --track-origins=yes --log-file="report-memecheck.txt" ./with-build/15-catch-me-if-you-can/build-catchme-Desktop_Qt_6_6_1_GCC_64bit-Debug/catchme
+
+echo "finished memcheck"
