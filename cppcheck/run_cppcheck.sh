471d2a94a3bcc42e89d1ca8124764df5a2210c3d
Natalija9 <asanovic.natalija@gmail.com>
2024-01-30 22:08:04 +0100

    Update script for cppcheck

--- cppcheck/run_cppcheck.sh
+++ cppcheck/run_cppcheck.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-cppcheck --quiet --inconclusive --enable=all --suppress=missingInclude --output-file="cppcheck-output.txt" ./without-build/15-catch-me-if-you-can
+cppcheck --quiet --inconclusive --enable=all --suppress=missingInclude --output-file="cppcheck-output.txt" ./15-catch-me-if-you-can
 
 echo "finished cppcheck"
